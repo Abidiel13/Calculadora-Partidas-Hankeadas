8b2920491576e8b48a88f20e47592e00510ec20f
Richard Amaral <master.of.rurouni@gmail.com>
2023-10-27 11:57:32 -0300

    Primeiro commit
    Primeiro código de uma calculadora para definir o nível de partidas rankeadas

---
+++ index.js
@@ -0,0 +1,27 @@
+function calcularNivel(vitorias, derrotas) {
+    const saldoVitorias = vitorias - derrotas;
+    let nivel = "";
+
+    if (vitorias < 10) {
+        nivel = "Ferro";
+    } else if (vitorias >= 10 && vitorias <= 20) {
+        nivel = "Bronze";
+    } else if (vitorias > 20 && vitorias <= 50) {
+        nivel = "Prata";
+    } else if (vitorias > 50 && vitorias <= 80) {
+        nivel = "Ouro";
+    } else if (vitorias > 80 && vitorias <= 90) {
+        nivel = "Diamante";
+    } else if (vitorias > 90 && vitorias <= 100) {
+        nivel = "Lendário";
+    } else {
+        nivel = "Imortal";
+    }
+
+    console.log(`O Herói tem um saldo de ${saldoVitorias} e está no nível de ${nivel}`);
+}
+
+
+const vitorias = 90; // Coloque o numero de vitorias.
+const derrotas = 30; // Coloque o numero de derrotas.
+calcularNivel(vitorias, derrotas);
